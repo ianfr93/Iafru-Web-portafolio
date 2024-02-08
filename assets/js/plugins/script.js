f4e0c3b8378bacdb33ec4254bfd88887c5a1aec9
ianfr93 <albitox10@gmail.com>
2024-02-08 00:45:09 -0300

    cambios

--- assets/js/plugins/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const progressBars = document.querySelectorAll(".progress-bar");
-
-    progressBars.forEach((bar) => {
-        const percent = bar.getAttribute("data-percent");
-        bar.style.width = percent;
-    });
-});
